0168bf06ba1de976aaf7965be819dcb5a589bce8
Patrik <patrik.veliki@presono.com>
2023-11-02 15:39:09 +0100

    Saving chartdata over ApiGateway to  MongoDB

--- Program.cs
+++ Program.cs
@@ -1,47 +1,41 @@
-using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MMLib.SwaggerForOcelot.DependencyInjection;
+using MMLib.SwaggerForOcelot.Middleware;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
 builder.Services.AddControllers();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api Gateway", Version = "v1" });
-});
 
 var app = builder.Build();
 
-// Enable CORS
+
 app.UseCors(builder => builder
-    .WithOrigins("http://localhost:5173") // Replace with the origin you want to allow
+    .WithOrigins("http://localhost:5173")
     .AllowAnyMethod()
     .AllowAnyHeader()
     .AllowCredentials());
 
-app.MapGet("/swagger ", () => Results.Redirect("/swagger"));
 app.MapControllers();
-
 await app.UseOcelot();
 
-// Configure the HTTP request pipeline.
-app.UseSwagger();
-app.UseSwaggerUI(c =>
+
+app.UseSwaggerForOcelotUI(opt =>
 {
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-    c.RoutePrefix = string.Empty; // Set the Swagger UI at the root URL
-});
+    opt.PathToSwaggerGenerator = "/swagger/docs";
+    opt.DownstreamSwaggerEndPointBasePath = "/swagger/docs";
+    opt.ServerOcelot = "/";
+}, swaggerUiOptions => { swaggerUiOptions.SwaggerEndpoint("/swagger/docs/v1/swagger.json", "My API V1"); });
 
 app.UseHttpsRedirection();
 app.UseRouting();
 app.UseAuthorization();
-
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllers();
-});
 
 app.Run();
